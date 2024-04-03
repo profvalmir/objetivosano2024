057fe8f4b1261c632159208a0a72dee791ed4b11
profvalmir <163314438+profvalmir@users.noreply.github.com>
2024-04-03 16:25:53 -0300

    main.js

--- main.js
+++ main.js
@@ -25,8 +25,7 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
-  let segundos = Math.floor(tempoFinal / 1000);
+    let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
@@ -34,10 +33,8 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-
+    
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
 }
 
 

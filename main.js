d4ce64bd49d5212962c03a8c5aeb38e0298bc718
profvalmir <163314438+profvalmir@users.noreply.github.com>
2024-04-10 15:39:55 -0300

    main.js

--- main.js
+++ main.js
@@ -18,8 +18,30 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo(tempos[0]); 
+
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
+
+
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -36,6 +58,11 @@ function calculaTempo(tempoObjetivo) {
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
 
+   if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 
 
     
